7f8443c267a29db97d1d673b88e8eb6462d75f54
Keyan Tabor <keyantabor98@gmail.com>
2023-06-20 11:19:31 -0400

    added socials and reworked about me

--- src/app/components/about/page.js
+++ src/app/components/about/page.js
@@ -1,20 +1,37 @@
+import Image from "next/image";
+import whitandi from "src/app/images/me-whit.jpg";
+
 export default function About() {
   return (
     <>
-    <div className="w-full h-screen">
-<div className="mockup-code">
-  <pre data-prefix="1"><code>npm i About-Me</code></pre> 
-  <pre data-prefix="2"><code>displaying...</code></pre>
-  <pre data-prefix="3">My name is Keyan Tabor, and I am a full-stack software developer.</pre>
-  <pre data-prefix="4">Directly after high school, I enlisted in the United States Marine Corps, serving as an 0311 Infantry Rifleman.</pre>
-  <pre data-prefix="5">This position not only allowed me to explore and appreciate a variety of global cultures</pre>
-  <pre data-prefix="6">but also offered me the chance to work with and lead teams within a large organizational structure. </pre>
-  <pre data-prefix="6">I was entrusted with the responsibility of maintaining and ensuring accountability for equipment and weapons worth millions of dollars. </pre>
-  <pre data-prefix="7">Following my discharge from the Marine Corps, I focused on furthering my education. </pre>
-  <pre data-prefix="8">I began with college studies and an internship in industrial mechanics before transitioning into a field that truly ignited my passion: software development.</pre>
-  <pre data-prefix="9">This discipline has enabled me to contribute significantly to areas I&apos;m fervent about and has redefined my professional path. </pre>
-</div>
-</div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row">
+          <Image
+            src={whitandi}
+            alt="picture of my wife and I"
+            className="max-w-sm rounded-lg shadow-2xl"
+          ></Image>
+          <div>
+            <h1 className="text-5xl font-bold">About Me</h1>
+            <p className="py-6">
+              My name is Keyan Tabor, and I am a full-stack software developer.
+              Directly after high school, I enlisted in the United States Marine
+              Corps, serving as an 0311 Infantry Rifleman. This position not
+              only allowed me to explore and appreciate a variety of global
+              cultures but also offered me the chance to work with and lead
+              teams within a large organizational structure. I was entrusted
+              with the responsibility of maintaining and ensuring accountability
+              for equipment and weapons worth millions of dollars. Following my
+              discharge from the Marine Corps, I focused on furthering my
+              education. I began with college studies and an internship in
+              industrial mechanics before transitioning into a field that truly
+              ignited my passion: software development. This discipline has
+              enabled me to contribute significantly to areas I'm fervent about
+              and has redefined my professional path.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/components/footer/page.js
+++ src/app/components/footer/page.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import github from "src/app/images/github-logo.svg";
+import linkedin from "src/app/images/linkedin-logo.svg";
+import twitter from "src/app/images/twitter-logo.svg";
+
 export default function Footer() {
-    return (
-        <footer className="  w-full flex justify-center absolute items-end bg-black text-gray-400">
-            <p>&#169;2023 Keyan Tabor All Rights Reserved</p>
-        </footer>
-    )
+  return (
+    <footer className="  w-full flex justify-center items-end bg-black text-gray-400">
+      <p className="pr-10">&#169;2023 Keyan Tabor All Rights Reserved</p>
+      <div className="flex justify-between md:place-self-center md:justify-self-end">
+        <Link href="https://github.com/Burmpf" target="_blank"> <Image src={github} alt="github logo" height={25} width={25}></Image> </Link>
+        <Link href="https://www.linkedin.com/in/keyan-tabor/" target="_blank"> <Image src={linkedin} alt="github logo" height={25} width={25}></Image> </Link>
+        <Link href="https://twitter.com/git_init_keyan" target="_blank"> <Image src={twitter} alt="github logo" height={25} width={25}></Image> </Link>
+      </div>
+    </footer>
+  );
 }
